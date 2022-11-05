cf0cdd025f64a3f1223c29818777546bf85801de
DESKTOP-DTMTFUL\thais <tsrossi@latam.stefanini.com>
2022-11-05 09:44:42 -0300

    criacao do ms login fora do gateway

---
+++ MsLogin/src/test/java/br/com/mov/securityapi/MsLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mov.securityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
